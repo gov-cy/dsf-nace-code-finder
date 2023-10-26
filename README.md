64446f31efc4c516c83bd8ca549e85866caa1b44
Constantinos Evangelou <cevangelou@dits.dmrid.gov.cy>
2023-10-26 08:27:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# DSF NACE code finder
+
+An easy tool to search for NACE codes
+
+All code is contained in the `/docs` folder.
+
+## Install and run
+
+You need to have [Node.js](https://nodejs.org/en/) installed.
+
+### 1. Download the tool
+
+### 2. Install modules
+
+Navigate to your local copies folder though the command line and Run the following command `npm install`
+
+### 3. Run
+
+Run the following command `npm start`
+
+It should load up the page in your browser.
---
+++ docs/js/app.js
@@ -0,0 +1,55 @@
+/**
+ * On window load
+ */
+$(window).on('load', function () {
+
+    //disable cache on ajax
+    $.ajaxSetup({ cache: false });
+
+    //get data from json
+    $.getJSON("data/nace_rev2.json", function (data) {
+
+        //filter for four digit codes only 
+        if ("geolocation" in navigator) {
+            
+        }
+        var filteredData = data.filter(filterFourDigit);
+
+        //the mustache template
+        var mustacheTemplate = "{{#.}}<tr><th>{{Code}}</th><td><b>Ελληνικά: </b>{{Description_el}} <br><b>English: </b>{{Description_en}}</td></tr>{{/.}}";
+        //render using mustache
+        var content = Mustache.render(mustacheTemplate, filteredData);
+
+        //render on page
+        $("#NACEBody").html(content);
+
+        //create datatable
+        $('#NICETable').DataTable({
+            responsive: true,
+            fixedHeader: true,
+            info: false,
+            ordering: false,
+            lengthChange: false,
+            // paging: false,
+            language: {
+                "search": "Αναζήτηση / Search <br>",
+                "zeroRecords": "Δεν βρέθηκαν αποτελέσματα / No matching records found",
+            },
+            "pageLength": 10
+        });
+    });
+
+});
+
+/**
+ * A function that returns true if the `item.Code` is a four digit number
+ * 
+ * @param {*} item the object to check for filtering 
+ * @returns boolean
+ */
+function filterFourDigit(item) {
+    if (item.Code < 10000) {
+      return true;
+    }
+    return false;
+  }
